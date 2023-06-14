90b124c8bfd405e0d448d87b2aac8f508e642a59
Ilya <Bossma1989@gmail.com>
2023-06-14 13:37:40 +0000

    new

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Man(object):
   satiety = 0
   happiness = 0
   food = 0
+  
 
   def eat():
     if food >= 20:
@@ -13,3 +14,7 @@ class Man(object):
       food -= 20
     else:
       print('Недостаточно еды! Сходите в магазин!')
+
+  def work():
+    money += 100
+    happines -= 40
