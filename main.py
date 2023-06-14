36a08e2c4d6e38249e824c52e6c985d7b80a4eec
Ilya <Bossma1989@gmail.com>
2023-06-14 13:21:36 +0000

    new

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-a = 1
+class Man(object):
+  name = 'Alex'
+  age = 0
+  money = 0
+  satiety = 0
+  happiness = 0
+
+  def eat():
+    if food >= 20:
+      satieti += 50
+      happiness += 20
+      food -= 20
+    else:
+      print('Недостаточно еды! Сходите в магазин!')
