0dcdca6239a3103be46b6f017d8f117844d47e50
Ilya <Bossma1989@gmail.com>
2023-06-16 13:27:44 +0000

    new

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import random
 
+
 class Man(object):
-  
+
   def __init__(self, name, age, satieti, money, happiness):
     self.name = name
     self.age = age
     self.satieti = satieti
     self.money = money
     self.happiness = happiness
-    
 
   def eat(self):
     global food
-    if food <= 20:
+    if 20 < food <= 40:
       self.go_to_the_store()
     else:
-      self.satieti = min(100, self.satieti+50)
-      self.happiness = min(100, self.happiness+20)
+      self.satieti = min(100, self.satieti + 50)
+      self.happiness = min(100, self.happiness + 20)
       food -= 20
-    
 
   def sleep(self):
     if self.satieti <= 20:
       self.eat()
     else:
-      self.happiness = min(100, self.happiness+50)
+      self.happiness = min(100, self.happiness + 50)
       self.satieti -= 20
 
   def go_to_work(self):
@@ -38,28 +37,23 @@ class Man(object):
     if self.satieti <= 20:
       self.eat()
     else:
-      self.happiness = min(100, self.happiness+60)
+      self.happiness = min(100, self.happiness + 60)
       self.satieti -= 20
 
   def go_to_the_store(self):
     global food
-    if self.money < 50:
-      self.go_to_work()    
-    elif self.happiness <= 20:
-      self.play_computer_games()
-    else:
-      food += 50
-      self.satieti -= 20
-      self.happiness -= 20
-      self.money -= 50
-    
+    food += 50
+    self.satieti -= 20
+    self.happiness -= 20
+    self.money -= 50
+
   def action(self, selected_action):
     action_dict = {
-    1: self.eat,
-    2: self.sleep,
-    3: self.go_to_work,
-    4: self.play_computer_games,
-    5: self.go_to_the_store
+      1: self.eat,
+      2: self.sleep,
+      3: self.go_to_work,
+      4: self.play_computer_games,
+      5: self.go_to_the_store
     }
     return action_dict[selected_action]()
 
@@ -69,37 +63,23 @@ class Man(object):
     elif self.satieti <= 0:
       return 'от голода. R.I.P.'
     else:
-      return 'от грусти. R.I.P.'  
-    
-    
+      return 'от грусти. R.I.P.'
 
-  
-    
+
 if __name__ == "__main__":
   character = Man('Товарищ', 25, 100, 100, 100)
   food = 0
-  for i in range(1,367):
-      if character.happiness > 0 and character.satieti > 0:
-        print(f'''День {i}
+  for i in range(1, 367):
+    if character.happiness > 0 and character.satieti > 0:
+      print(f'''День {i}
 {character.name} - {character.age+i//365}
 Счастье - {character.happiness}
 Сытость - {character.satieti}
 Деньги - {character.money}
-''')    
-        character.action(random.randint(1,5))
-        print()
-      else:
-        print(f'''День {i}
+''')
+      character.action(random.randint(1, 5))
+      print()
+    else:
+      print(f'''День {i}
 {character.name} умер {character.couse_of_dead()}''')
-        break
-    
-    
-    
-  
-
-  
-  
-  
-  
-  
-  
+      break
