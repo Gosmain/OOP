3c42f53743609106fa4701e60c4cf29334bdfc75
Ilya <Bossma1989@gmail.com>
2023-07-14 10:17:38 +0000

    TODO

--- models/food.py
+++ models/food.py
@@ -11,11 +11,11 @@ class Food(object):
   def __str__(self):
     return f'Количество еды {self.value}.'
 
-  def chance_spoil(self):
+  def get_chance_spoil(self):
     return random.randint(1, 10) <= food_config.SPOIL_FOOD_CHANCE
 
   def spoil(self):
-    if self.chance_spoil():
+    if self.get_chance_spoil():
       self.value = max(food_config.MIN_FOOD_VALUE,
                        self.value - food_config.SPOIL_FOOD_STEP)
 
